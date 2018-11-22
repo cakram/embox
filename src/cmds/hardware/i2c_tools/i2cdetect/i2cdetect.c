8cd6f1d42c67e3e30c8a0a0b4ad17fa1c5332d89
Anton Bondarev <anton.bondarev2310@gmail.com>
2018-11-22 16:48:51 +0300

    cmds: Work on i2c_tools/i2cdetect

---
+++ src/cmds/hardware/i2c_tools/i2cdetect/i2cdetect.c
@@ -0,0 +1,77 @@
+/**
+ * @file
+ *
+ * @date Nov 16, 2018
+ * @author Anton Bondarev
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+static void print_usage(void) {
+	printf("Usage: i2cdetect -h\n");
+	printf("Usage: i2cdetect -l\n");
+	printf("Usage: i2cdetect [-y] i2cbus\n");
+	printf("Usage: i2cdetect -F i2cbus\n");
+}
+
+static void print_error(void) {
+	printf("Wrong parameters\n");
+}
+
+extern int imx_i2c_receive(uint16_t addr, uint8_t *ch) ;
+
+static void i2c_bus_list(void) {
+
+}
+
+static void i2c_bus_scan(long busn) {
+	int i;
+	uint8_t tmp;
+
+	printf("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n");
+	printf("00:         ");
+	for (i = 0x03; i < 0x77; i++) {
+		if (0 == (i % 0x10)) {
+			printf("\n%2X:", (unsigned)i);
+		}
+		if (0 < imx_i2c_receive(i, &tmp)) {
+			printf(" %2X", (unsigned)i);
+		} else {
+			printf(" --");
+		}
+	}
+
+	printf("\n");
+}
+
+int main(int argc, char **argv) {
+	int opt;
+	long busn = 0;
+
+
+	while (-1 != (opt = getopt(argc, argv, "hly:F:"))) {
+		switch (opt) {
+		case 'l':
+			i2c_bus_list();
+			return 0;
+		case 'y':
+			busn = strtol(optarg, NULL, 0);
+			i2c_bus_scan(busn);
+			return 0;
+		case 'F':
+			busn = strtol(optarg, NULL, 0);
+			return 0;
+		case '?':
+		case 'h':
+			print_usage();
+			return 0;
+		default:
+			print_error();
+			return 0;
+		}
+	}
+}
